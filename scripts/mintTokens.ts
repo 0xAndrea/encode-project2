6a878ab5d4ac7efed527d6780a5d97af3abbf368
0xAndrea <94756464+0xAndrea@users.noreply.github.com>
2022-05-30 13:00:07 +0100

    Update mintTokens.ts

--- scripts/mintTokens.ts
+++ scripts/mintTokens.ts
@@ -6,7 +6,13 @@ import { connectToBlockchain } from "./utils"
 
 async function main() {
     const signer = await connectToBlockchain();
-    const myTokenAddress = "0x0c6812bB78246540d5208f0e9E7e595eC45A6Cd5";
+    let myTokenAddress;
+    if (process.argv.length < 2) {
+        myTokenAddress = "0x0c6812bB78246540d5208f0e9E7e595eC45A6Cd5";
+    }
+    else {
+        myTokenAddress = process.argv[2];
+    }
 
 
     const userAddress = process.argv[2];
